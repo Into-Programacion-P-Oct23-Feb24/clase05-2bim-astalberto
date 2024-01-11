ca4ae5fefcc3be9eb55d0af0c0ddee7abc4dfd00
astalberto <albertoherrera205@hotmail.com>
2024-01-11 15:51:44 -0500

    Push

---
+++ EjemplosRecursividad/src/paquete03/Ejemplo01.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paquete03;
+
+/**
+ *
+ * @author alber
+ */
+public class Ejemplo01 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static String[][] arreglo() {
+        String[][] arreglo2 = {{"lOJA","CUENCA","Quito"},{"Zamora","Guayaquill","Zapotillo"}};
+        return arreglo2; 
+    }
+}
